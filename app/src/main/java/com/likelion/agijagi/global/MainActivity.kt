01bcb274b995e3b10100f31220193eef654399fa
june0103 <rhwns0103@naver.com>
2023-12-05 11:02:20 +0900

    Feat: 바텀네비게이션 및 테마 설정 #5

--- app/src/main/java/com/likelion/agijagi/global/MainActivity.kt
+++ app/src/main/java/com/likelion/agijagi/global/MainActivity.kt
@@ -1,12 +1,70 @@
 package com.likelion.agijagi.global
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.likelion.agijagi.R
+import com.likelion.agijagi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var activityMainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        onSetUpNavigation()
+    }
+
+    private fun onSetUpNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        activityMainBinding.bottomNavigation.apply {
+            setupWithNavController(navController)
+
+            setOnItemSelectedListener { item ->
+//                if (UserModel.uid == "" && (item.itemId == R.id.orderFragment ||
+//                            item.itemId == R.id.wishListFragment ||
+//                            item.itemId == R.id.buyerMypageFragment)
+//                ) {
+//                    displayDialogUserNotLogin(
+//                        this@MainActivity,
+//                        navController,
+//                        R.id.loginFragment
+//                    )
+//                    return@setOnItemSelectedListener false
+//                }
+
+                NavigationUI.onNavDestinationSelected(item, navController)
+                navController.popBackStack(item.itemId, inclusive = false)
+                true
+            }
+            navController.addOnDestinationChangedListener { _, destination, _ ->
+                visibility = when (destination.id) {
+                    R.id.homeFragment,
+                    R.id.categoryFragment,
+                    R.id.orderFragment,
+                    R.id.wishListFragment,
+                    R.id.buyerMypageFragment -> {
+                        View.VISIBLE
+                    }
+
+                    else -> {
+                        View.GONE
+                    }
+                }
+            }
+        }
     }
 }
